e77f52a4cbf70a5397ba861fcbfe429ed6f35f16
Rommel Fernando <rommelfernando@Rommels-MacBook-Pro.local>
2018-03-07 21:30:39 -0800

    update syntax

--- config/orm.js
+++ config/orm.js
@@ -1,29 +1,97 @@
 const connection = require("../config/connection.js");
 
+function printQuestionMarks(num) {
+    var arr = [];
+  
+    for (var i = 0; i < num; i++) {
+      arr.push("?");
+    }
+  
+    return arr.toString();
+  }
+  
+  function objToSql(ob) {
+    var arr = [];
+  
+    for (var key in ob) {
+      var value = ob[key];
+      if (Object.hasOwnProperty.call(ob, key)) {
+        if (typeof value === "string" && value.indexOf(" ") >= 0) {
+          value = "'" + value + "'";
+        }
+        arr.push(key + "=" + value);
+      }
+    }
+  
+    return arr.toString();
+  }
+  
+  // Object for all our SQL statement functions.
+  var orm = {
+    all: function(tableInput, cb) {
+      var queryString = "SELECT * FROM " + tableInput + ";";
+      connection.query(queryString, function(err, result) {
+        if (err) {
+          throw err;
+        }
+        cb(result);
+      });
+    },
+    create: function(table, cols, vals, cb) {
+      var queryString = "INSERT INTO " + table;
+  
+      queryString += " (";
+      queryString += cols.toString();
+      queryString += ") ";
+      queryString += "VALUES (";
+      queryString += printQuestionMarks(vals.length);
+      queryString += ") ";
+  
+      console.log(queryString);
+  
+      connection.query(queryString, vals, function(err, result) {
+        if (err) {
+          throw err;
+        }
+  
+        cb(result);
+      });
+    },
 
-// In the orm.js file, create the methods 
-// that will execute the necessary MySQL commands in the controllers. 
-// These are the methods you will need to use in order to retrieve and store data in your database.
-// selectAll()
-// insertOne()
-// updateOne()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    update: function(table, objColVals, condition, cb) {
+      var queryString = "UPDATE " + table;
+  
+      queryString += " SET ";
+      queryString += objToSql(objColVals);
+      queryString += " WHERE ";
+      queryString += condition;
+  
+      console.log(queryString);
+      connection.query(queryString, function(err, result) {
+        if (err) {
+          throw err;
+        }
+  
+        cb(result);
+      });
+    },
+  
+      delete: function(table, objColVals, condition, cb) {
+        var queryString = "DELETE " + table;
+    
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+    
+        console.log(queryString);
+        connection.query(queryString, function(err, result) {
+          if (err) {
+            throw err;
+          }
+          cb(result);
+        });
+      }
+  };
 
 module.exports = orm;
--- models/burger.js
+++ models/burger.js
@@ -2,18 +2,18 @@ const orm = require('../config/orm.js');
 
 const burger = {
     selectAll: function(cb) {
-      orm.all('burgers', function(res) {
+      orm.selectAll('burgers', function(res) {
         cb(res);
       });
     },
     
     insertOne: function(cols, vals, cb) {
-      orm.create('burgers', cols, vals, function(res) {
+      orm.insertOne('burgers', cols, vals, function(res) {
         cb(res);
       });
     },
     updateOne: function(objColVals, condition, cb) {
-      orm.update('burgers', objColVals, condition, function(res) {
+      orm.updateOne('burgers', objColVals, condition, function(res) {
         cb(res);
       });
     }
