d32db8148895379bdde210f52a19831cb5539ab7
Rommel Fernando <rommelfernando@Rommels-MacBook-Pro.local>
2018-03-11 23:09:01 -0700

    updated function syntax

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,21 @@
 // Set up MySQL connection.
-const mysql = require("mysql");
+const mysql = require('mysql');
 
 const connection = mysql.createConnection({
   port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "Music6247",
-  database: "burger_db"
+  host: 'localhost',
+  user: 'root',
+  password: 'Music6247',
+  database: 'burgers_db'
 });
 
 // Make connection.
-connection.connect(function(err) {
+connection.connect((err) => {
   if (err) {
-    console.error("error connecting: " + err.stack);
+    console.error('error connecting: ' + err.stack);
     return;
   }
-  console.log("connected as id " + connection.threadId);
+  console.log('connected as id ' + connection.threadId);
 });
 
 // Export connection for our ORM to use.
