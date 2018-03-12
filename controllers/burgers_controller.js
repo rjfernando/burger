fe34b06a7c021b0cd5a7505cc89e6529192426ea
Rommel Fernando <rommelfernando@Rommels-MacBook-Pro.local>
2018-03-11 21:25:29 -0700

    corrected the post function

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -9,31 +9,27 @@ router.get('/', function (req, res) {
         var hbsObject = {
             burgers: data
         };
-        console.log(hbsObject);
         res.render('index', hbsObject);
     });
 });
 
-router.post("/", function (req, res) {
+router.post('/', function (req, res) {
     burger.insertOne([
-        "burger_name", "devoured"
+        'burger_name'
     ], [
-        req.body.name, false
+        req.body.burger_name
     ], function () {
-        res.redirect("/");
+        res.redirect('/');
     });
 });
 
 router.put('/:id', function (req, res) {
     var condition = 'id = ' + req.params.id;
     burger.updateOne({
-            devoured: req.body.devoured
-        },
-        condition,
-        function (data) {
+            devoured: true
+        }, condition, function (data) {
             res.redirect('/');
-        }
-    );
+    });   
 });
 
 module.exports = router;
